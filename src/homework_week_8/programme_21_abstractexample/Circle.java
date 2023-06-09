3835711c8a0bdfc9bcaee80daa0b9465bae629d4
ashish1patel <pashish.18mail@gmail.com>
2023-06-09 08:25:31 +0100

    homework submitted

---
+++ src/homework_week_8/programme_21_abstractexample/Circle.java
@@ -0,0 +1,7 @@
+package homework_week_8.programme_21_abstractexample;
+
+public class Circle extends Shape{
+    public void draw(){
+        System.out.println("drawing circle");
+    }
+}
---
+++ src/homework_week_8/programme_24_method_overriding/SBI.java
@@ -0,0 +1,7 @@
+package homework_week_8.programme_24_method_overriding;
+
+public class SBI extends Bank{
+    public int getRateOfInterest() {
+        return 8;
+    }
+}
